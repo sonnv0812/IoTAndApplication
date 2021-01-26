f6f23a30c890c9772b5a88c2df9ad3cef0c293ed
sonnv0812 <vansonducchinh@gmail.com>
2021-01-27 05:10:16 +0700

    notify

--- app/src/main/java/com/example/projectiot/service/AlarmMusic.java
+++ app/src/main/java/com/example/projectiot/service/AlarmMusic.java
@@ -1,12 +1,20 @@
 package com.example.projectiot.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 import com.example.projectiot.R;
 
@@ -21,6 +29,26 @@ public class AlarmMusic extends Service {
         return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        String NOTIFICATION_CHANNEL_ID = "com.example.simpleapp";
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setContentTitle("Cháy nhà rồi")
+                .build();
+        startForeground(2, notification);
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         co = intent.getExtras().getFloat("co");
@@ -29,7 +57,7 @@ public class AlarmMusic extends Service {
         if (co > 100) {
             mediaPlayer.start();
         }
-        return START_NOT_STICKY;
+        return START_STICKY;
     }
 
     @Override
--- app/src/main/java/com/example/projectiot/ui/main/MainActivity.java
+++ app/src/main/java/com/example/projectiot/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.projectiot.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,10 +14,13 @@ import com.example.projectiot.data.model.DataSensor;
 import com.example.projectiot.data.repository.main.MainRepository;
 import com.example.projectiot.data.repository.main.MainRepositoryImpl;
 import com.example.projectiot.service.AlarmMusic;
+import com.example.projectiot.ui.login.LoginActivity;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
@@ -26,6 +30,8 @@ public class MainActivity extends AppCompatActivity
     private MainContract.Presenter presenter;
     private LineChart chartCoLpgSmoke, chartHumidity, chartTemperature;
     private long startTime;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
     float co = 0, lpg = 0, smoke = 0, humidity = 0, temperature = 0;
     Intent intent;
     ArrayList<Entry> dataCo = new ArrayList<Entry>();
@@ -50,12 +56,41 @@ public class MainActivity extends AppCompatActivity
         chartHumidity.getDescription().setText("Biểu đồ độ ẩm không khí");
         chartTemperature.getDescription().setText("Biểu đồ nhiệt độ");
 
+        auth = FirebaseAuth.getInstance();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    Log.v("MAIN", "TAO DANG O DAY");
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
 
     }
 
     private void initPresenter() {
         MainRepository repository = new MainRepositoryImpl();
         presenter = new MainPresenter(this, repository);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
     }
 
     @Override
@@ -90,7 +125,6 @@ public class MainActivity extends AppCompatActivity
         humidity = data.getHumidity();
         temperature = data.getTemperature();
 
-        Log.v("MAIN", time + " " + startTime);
         dataCo.add(new Entry(pointCo.getxValue(), pointCo.getyValue()));
         dataLpg.add(new Entry(pointLpg.getxValue(), pointLpg.getyValue()));
         dataSmoke.add(new Entry(pointSmoke.getxValue(), pointSmoke.getyValue()));
